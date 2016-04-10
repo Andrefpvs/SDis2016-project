d704dd267d90edeedc45ffa1cb343767e141295f
André Silva <andrefpvs@netcabo.pt>
2016-04-10 18:24:27 +0100

    Add Broker Server "skeleton"

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,49 @@
 package pt.upa.broker.ws;
 
-public class BrokerPort {
+import java.util.List;
+import javax.jws.WebService;
 
-	// TODO
+@WebService(
+	    endpointInterface="pt.upa.broker.ws.BrokerPortType",
+	    wsdlLocation="broker.1_0.wsdl",
+	    name="UpaBroker",
+	    portName="BrokerPort",
+	    targetNamespace="http://ws.broker.upa.pt/",
+	    serviceName="BrokerService"
+)
 
+public class BrokerPort implements BrokerPortType {
+
+	@Override
+	public String ping(String name) {
+		// TODO Actually implement the method
+		return "PONG!";
+	}
+
+	@Override
+	public String requestTransport(String origin, String destination, int price)
+			throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
+			UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+		// TODO Actually implement the method
+		return "Here's your transport";
+	}
+
+	@Override
+	public TransportView viewTransport(String id) throws UnknownTransportFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TransportView> listTransports() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearTransports() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
